0d06e62acadb1a37011369b8a7ae9bdea74ffd37
Accurics <admin@accurics.com>
2022-02-16 13:50:02 -0500

    Auto Remediation By Accurics

--- aws/s3_bucket.tf
+++ aws/s3_bucket.tf
@@ -3,3 +3,27 @@ resource "aws_s3_bucket" "tenable_cs_demo_s3_bucket" {
   acl    = "public-read"
   tags   = var.default_tags
 }
+resource "aws_s3_bucket_policy" "tenable_cs_demo_s3_bucketPolicy" {
+  bucket = "${aws_s3_bucket.tenable_cs_demo_s3_bucket.id}"
+
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "tenable_cs_demo_s3_bucket-restrict-access-to-users-or-roles",
+      "Effect": "Allow",
+      "Principal": [
+        {
+          "AWS": [
+            <aws_policy_role_arn>
+          ]
+        }
+      ],
+      "Action": "s3:GetObject",
+      "Resource": "arn:aws:s3:::${aws_s3_bucket.tenable_cs_demo_s3_bucket.id}/*"
+    }
+  ]
+}
+POLICY
+}
